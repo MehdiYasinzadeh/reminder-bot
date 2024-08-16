266118f48d0718854e51bc3fcfe068e57b1941b6
shakila_jelvani <7shjelvani7@gmail.com>
2024-08-16 20:33:24 +0330

    add jwt configuration

---
+++ birthday-service/src/main/java/ir/reminder/auth/AuthenticationRequest.java
@@ -0,0 +1,17 @@
+package ir.reminder.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationRequest {
+
+    private Long chatId;
+    private String password;
+
+}
---
+++ birthday-service/src/main/java/ir/reminder/entity/Role.java
@@ -0,0 +1,5 @@
+package ir.reminder.entity;
+
+public enum Role {
+    ADMIN,USER
+}
